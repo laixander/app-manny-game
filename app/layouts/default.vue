a939c4457c99ea8c8c55028957e9d0ff85de189d
laixander <laixander@gmail.com>
2025-11-16 22:21:23 +0800

    updates

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-800">
+        <div class="mx-auto max-w-md bg-white min-h-screen relative">
+            <div class="p-6 pb-24 min-h-screen bg-gradient-to-b from-[#1c58a7] relative size-full to-[#092b59]">
+                <!-- <Header /> -->
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="maiden-orange-font text-white text-3xl text-center">
+        Where is
+    </div>
+    <div class="relative">
+
+        <div class="luckiest-guy-font text-white absolute -top-[5px] -left-[5px] uppercase text-7xl text-center -tracking-[8px]">
+            Manny?
+        </div>
+        <div class="luckiest-guy-font text-transparent bg-clip-text bg-gradient-to-r from-[#f23030] to-[#6f0305] absolute uppercase text-6xl text-center">
+            Manny?
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
